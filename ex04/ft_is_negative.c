ef0083cce80fbfea6aac219332ad54a859380405
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-07-27 17:23:48 +0800

    ex04. TODO - for n is also null.

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -6,17 +6,23 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:43:38 by pteh              #+#    #+#             */
-/*   Updated: 2023/07/27 16:44:42 by pteh             ###   ########.fr       */
+/*   Updated: 2023/07/27 17:22:41 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include<unistd.h>
 
 int	ft_is_negative(int n)
 {
-	
-}
+	char	a;
 
-int	main(void)
-{
-	ft_is_negative(8);
+	if (n > 0)
+	{
+		a = 'P';
+		write(1,&a,1);
+	} else if (n < 0)
+	{
+		a = 'N';
+		write(1,&a,1);
+
+	}	
 }
