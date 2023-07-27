3de4198f7637c6f9793e33ab3aae05172a14c4c4
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-07-27 17:12:11 +0800

    refactored ex03, removed norminette error

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 16:27:53 by pteh              #+#    #+#             */
-/*   Updated: 2023/07/27 16:39:51 by pteh             ###   ########.fr       */
+/*   Updated: 2023/07/27 17:11:17 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,11 @@
 int	ft_print_numbers(void)
 {
 	char	number;
-	
+
 	number = 47;
 	while (number <= 57)
 	{
 		number++;
 		write(1, &number, 1);
 	}
-}
-
-int	main(void)
-{
-	ft_print_numbers();
 }
